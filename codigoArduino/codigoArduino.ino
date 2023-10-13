568caa4b02921114eba7cdd27708876d89d04c22
Leticia Daniele Perotti <80782049+leticia-perotti@users.noreply.github.com>
2023-10-12 22:13:43 -0300

    Código arduino pronto

--- codigoArduino/codigoArduino.ino
+++ codigoArduino/codigoArduino.ino
@@ -30,7 +30,30 @@ void setup() {
 void loop() {
   Serial.print("luminosidade:"+String(analogRead(ldr)));
 
-  delay(5000);
+  controleDht.read11(pinoDht);
+
+  Serial.print("umidade:"+String(controleDht.humidity)+"|"); 
+  
+  Serial.print("temperatura:"+String(controleDht.temperature)+"|"); 
+
+  if (!rfid.PICC_IsNewCardPresent() || !rfid.PICC_ReadCardSerial())
+    return;
+ 
+  String strID = ""; 
+  for (byte i = 0; i < 4; i++) {
+    strID +=
+    (rfid.uid.uidByte[i] < 0x10 ? "0" : "") +
+    String(rfid.uid.uidByte[i], HEX) +
+    (i!=3 ? ":" : "");
+  }
+  strID.toUpperCase();
+ 
+  Serial.println("cartaoacesso:"+strID+"|");
+ 
+  rfid.PICC_HaltA();
+  rfid.PCD_StopCrypto1();
+
+  delay(1000);
 /*
   if (!rfid.PICC_IsNewCardPresent() || !rfid.PICC_ReadCardSerial()) //VERIFICA SE O CARTÃO PRESENTE NO LEITOR É DIFERENTE DO ÚLTIMO CARTÃO LIDO. CASO NÃO SEJA, FAZ
     return; //RETORNA PARA LER NOVAMENTE
